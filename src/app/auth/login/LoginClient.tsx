bfcef028e19e3d17fd7544e1f0988836189bb877
AC.alejocardozo <121801711+Cone223@users.noreply.github.com>
2026-04-28 10:25:13 -0300

    Add LoginClient component for login page

---
+++ src/app/auth/login/LoginClient.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function LoginClient() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      Login page
+    </div>
+  )
+}
